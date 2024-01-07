50b3ff5a8ae397a65525e6d2dcd9bef9d1b1191a
Théo Monnom <theo.8bits@gmail.com>
2024-01-07 04:12:40 +0100

    tests

--- src/sys/x11/mod.rs
+++ src/sys/x11/mod.rs
@@ -6,9 +6,8 @@ mod sys {
 
 #[cfg(test)]
 mod tests {
-    use std::mem::MaybeUninit;
-
     use super::*;
+    use std::mem::MaybeUninit;
 
     #[test]
     fn test_x11() {
@@ -17,6 +16,25 @@ mod tests {
 
             let mut session = MaybeUninit::uninit();
             let error = sys::new_session(session.as_mut_ptr());
+            let session = session.assume_init();
+
+            println!("session: {:?}", session);
+            println!("error: {}", error);
+
+            let mut screens = MaybeUninit::uninit();
+            let mut count = MaybeUninit::uninit();
+            sys::list_screens(session, screens.as_mut_ptr(), count.as_mut_ptr());
+            let screens = screens.assume_init();
+            let count = count.assume_init();
+
+            println!("screens: {:?}", screens);
+            println!("count: {}", count);
+
+            for i in 0..count {
+                // dereference screen
+                let screen = *screens.add(i as usize);
+                println!("screen: {:?}", screen);
+            }
         }
     }
 }
